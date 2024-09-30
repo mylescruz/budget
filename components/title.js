c5bc084e55927463a8802df5f68cacc56e6a9690
Myles <mylesvirgilcruz@gmail.com>
2024-09-29 17:56:52 -0700

    Add project files to GitHub repository

---
+++ components/title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <header className="container mt-4">
+            <h1>BUDGET</h1>
+        </header>
+    );
+};
+
+export default Title;
---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import Title from "@/components/title";
+import "@/styles/globals.css";
+
+const App = () => {
+    return (
+      <>
+        <Title />
+      </>
+    );
+  };
+  
+export default App;
