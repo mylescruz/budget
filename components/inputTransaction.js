b301ab34606f0861a4b9c742cf61031371db51f0
Myles <mylesvirgilcruz@gmail.com>
2024-10-02 09:56:14 -0700

    Fix empty and invalid input bugs in inputTransaction form

--- components/inputTransaction.js
+++ components/inputTransaction.js
@@ -1,19 +1,8 @@
 import { useState } from "react";
 import styles from "@/styles/inputTransaction.module.css";
-import { Card, Container } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 const InputTransaction = ({transactions, updateTransactions, categories}) => {
-    const emptyTransaction = {
-        id: 999,
-        date: "",
-        store: "",
-        items: "",
-        category: "",
-        amount: 0
-    };
-
-    const [newTransaction, setTransaction] = useState(emptyTransaction);
-    
     const currentDate = new Date();
     const currentMonth = currentDate.getMonth() + 1;
     const currentYear = currentDate.getFullYear();
@@ -22,74 +11,108 @@ const InputTransaction = ({transactions, updateTransactions, categories}) => {
     const minDate = startOfMonth.toISOString().split('T')[0];
     const maxDate = endOfMonth.toISOString().split('T')[0];
 
-    const AddTransaction = () => {
-        const maxID = Math.max(...transactions.map(trans => trans.id));
+    const emptyTransaction = {
+        id: 0,
+        date: minDate,
+        store: "",
+        items: "",
+        category: "Rent",
+        amount: ""
+    };
+
+    const [newTransaction, setTransaction] = useState(emptyTransaction);
+
+    const handleInput = (e) => {
+        setTransaction({ ...newTransaction, [e.target.id]: e.target.value});
+    };
+
+    const handleNumInput = (e) => {
+        const input = e.target.value;
+
+        if (input == '')
+            setTransaction({ ...newTransaction, amount: input });
+        else
+            setTransaction({ ...newTransaction, amount: parseFloat(input) });
+    };
+
+    const AddTransaction = (e) => {
+        e.preventDefault();
+        
+        let maxID = 0;
+        if (transactions.length > 0)
+            maxID = Math.max(...transactions.map(trans => trans.id));
+
         newTransaction.id = maxID + 1;
         updateTransactions(newTransaction);
         setTransaction(emptyTransaction);
     };
 
     return (
-        <Card className={styles.inputCard}>
-            <div className="col-5">
-                <input
+        <Form className="card w-25" onSubmit={AddTransaction}>
+            <Form.Group className={styles.formInput}>
+                <Form.Control
                     id="date"
                     className="h-100"
                     type="date"
                     min={minDate}
                     max={maxDate}
-                    onChange={(e) => setTransaction({ ...newTransaction, date: e.target.value })}
+                    value={newTransaction.date}
+                    onChange={handleInput}
                     required
-                ></input>
-            </div>
-            <div className="col-5">
-                <input
+                ></Form.Control>
+            </Form.Group>
+            <Form.Group className={styles.formInput}>
+                <Form.Control
                     id="store"
                     className="h-100"
                     type="text"
                     placeholder="Store/Restaurant"
-                    onChange={(e) => setTransaction({ ...newTransaction, store: e.target.value })}
+                    value={newTransaction.store}
+                    onChange={handleInput}
                     required
-                ></input>
-            </div>
-            <div className="col-5">
-                <input
+                ></Form.Control>
+            </Form.Group>
+            <Form.Group className={styles.formInput}>
+                <Form.Control
                     id="items"
                     className="h-100"
                     type="text"
                     placeholder="What was purchased?"
-                    onChange={(e) => setTransaction({ ...newTransaction, items: e.target.value })}
+                    value={newTransaction.items}
+                    onChange={handleInput}
                     required
-                ></input>
-            </div>
-            <div className="col-5">
-                <select id="category" className="h-100" 
-                onChange={(e) => setTransaction({ ...newTransaction, category: e.target.value })}
+                ></Form.Control>
+            </Form.Group>
+            <Form.Group className={styles.formInput}>
+                <Form.Select id="category" className="h-100" 
+                value={newTransaction.category}
+                onChange={handleInput}
                 required>
                     <option disabled>Choose a Category...</option>
                     {categories.map(category => (
                         <option key={category.id} value={category.name}>{category.name}</option>
                     ))}
-                </select>
-            </div>
-            <div className="col-5">
-                <input
+                </Form.Select>
+            </Form.Group>
+            <Form.Group className={styles.formInput}>
+                <Form.Control
                     id="amount"
                     className="h-100"
                     type="number"
                     min="0.01"
                     step="0.01"
                     placeholder="Amount"
-                    onChange={(e) => setTransaction({ ...newTransaction, amount: parseFloat(e.target.value) })}
+                    value={newTransaction.amount}
+                    onChange={handleNumInput}
                     required
-                ></input>
-            </div>
-            <div className="col-2">
-                <button className="btn btn-primary" onClick={AddTransaction}>
+                ></Form.Control>
+            </Form.Group>
+            <Form.Group className={styles.formInput}>
+                <Button type="submit" className="btn btn-primary">
                     Add
-                </button>
-            </div>
-        </Card>
+                </Button>
+            </Form.Group>
+        </Form>
     );
 };
 
