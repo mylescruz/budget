305e580dd293e7d9a354786c3b9fc3b07d93ce33
Myles <mylesvirgilcruz@gmail.com>
2024-12-02 00:35:23 -0800

    Update budget for the month to be based on the income given

--- components/month/categoryTable/categoryFooter.js
+++ components/month/categoryTable/categoryFooter.js
@@ -1,21 +1,30 @@
 import { CategoriesContext } from "@/contexts/CategoriesContext";
 import currencyFormatter from "@/helpers/currencyFormatter";
+import dateFormatter from "@/helpers/dateFormatter";
+import dateInfo from "@/helpers/dateInfo";
+import usePaystubs from "@/hooks/usePaystubs";
 import { useContext, useMemo } from "react";
 
 const CategoryFooter = () => {   
     const { categories } = useContext(CategoriesContext); 
+    const { paystubs } = usePaystubs(dateInfo.currentYear);
     
     const footerValues = useMemo(() => {
         let totalBudget = 0;
-        let totalActual = 0;
+        paystubs.map(paystub => {
+            const paystubDate = new Date(dateFormatter(paystub.date));
+            const paystubMonth = paystubDate.toLocaleDateString('default', {month: 'long'});
+            if (paystubMonth === dateInfo.currentMonth)
+                totalBudget += paystub.net;
+        });
 
+        let totalActual = 0;
         categories.forEach(category => {
-            totalBudget += category.budget;
             totalActual += category.actual;
         });
 
         return {budget: totalBudget, actual: totalActual};
-    }, [categories]);
+    }, [categories, paystubs]);
 
     const totalDifference = footerValues.budget - footerValues.actual;
 
