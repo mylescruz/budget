74df77161ea37fc6601f71d9e42b9952d628624c
Myles <mylesvirgilcruz@gmail.com>
2025-02-04 16:13:44 -0800

    Updated styling of history page

--- components/history/historyTable.js
+++ components/history/historyTable.js
@@ -1,11 +1,11 @@
 import { Table } from "react-bootstrap";
 import currencyFormatter from "@/helpers/currencyFormatter";
 import Link from "next/link";
-import getMonthInfo from "@/helpers/getMonthInfo";
+import styles from "@/styles/historyTable.module.css";
 
 const HistoryTable = ({ history }) => {
     return (
-        <Table bordered className="w-75 mx-auto">
+        <Table bordered hover className={`w-75 mx-auto ${styles.table}`}>
             <thead className="table-dark">
                 <tr>
                     <th scope="col">Month</th>
@@ -16,11 +16,14 @@ const HistoryTable = ({ history }) => {
             </thead>
             <tbody>
                 {history.map(month => (
-                    <tr key={month.id}>
+                    <tr key={month.id} className={styles.cell}>
                         <td><Link href={{pathname:'/history/[month]', query: {month: month.month, year: month.year}}}>{month.month} {month.year}</Link></td>
                         <td>{currencyFormatter.format(month.budget)}</td>
                         <td>{currencyFormatter.format(month.actual)}</td>
+                        {month.leftover >= 0 ? 
                         <td>{currencyFormatter.format(month.leftover)}</td>
+                        :
+                        <td className="text-danger">{currencyFormatter.format(month.leftover)}</td>}
                     </tr>
                 ))}                
             </tbody>
