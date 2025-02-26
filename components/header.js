83db5a066c91392ee0570bb4b3713a5032671180
Myles <mylesvcruz@gmail.com>
2025-02-26 14:57:22 -0800

    Updated header component to check if a user has a session

--- components/header.js
+++ components/header.js
@@ -1,22 +1,50 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
-    return (
-        <Navbar expand="lg" bg="dark" data-bs-theme="dark" fixed="top">
-        <Container>
-            <Navbar.Brand href="/">Type-A Budgeting</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link as={Link} href="/budget">Budget</Nav.Link>
-                <Nav.Link as={Link} href="/income">Income</Nav.Link>
-                <Nav.Link as={Link} href="/history">History</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
-        </Navbar>
-    );
+    const { data: session } = useSession();
+
+    const userSignIn = async () => {
+        await signIn({ callbackUrl: '/'});
+    };
+
+    const userSignOut = async () => {
+        await signOut({ callbackUrl: '/'});
+    };
+
+    if (session) {
+        return (
+            <Navbar expand="lg" bg="dark" data-bs-theme="dark" fixed="top">
+                <Container>
+                    <Navbar.Brand href="/">Type-A Budget</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} href="/budget">Budget</Nav.Link>
+                            <Nav.Link as={Link} href="/income">Income</Nav.Link>
+                            <Nav.Link as={Link} href="/history">History</Nav.Link>
+                            <Nav.Link onClick={userSignOut}>Logout</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        );
+    } else {
+        return (
+            <Navbar expand="lg" bg="dark" data-bs-theme="dark" fixed="top">
+                <Container>
+                    <Navbar.Brand href="/">Type-A Budget</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link onClick={userSignIn}>Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        );
+    }
 };
 
 export default Header;
