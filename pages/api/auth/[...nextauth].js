e7f5d42de1dd766eaa4cdf953fb537a5177551c7
Myles <mylesvcruz@gmail.com>
2025-02-26 14:04:55 -0800

    Installed NextAuth.js and initialized [...nextauth].js file

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                try {
+                    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/user/authorize`, {
+                        method: 'POST',
+                        body: JSON.stringify(credentials),
+                        headers: { "Content-Type": "application/json" }
+                    });
+                    const user = await res.json();
+                
+                    if (res.ok && user) {
+                        return user;
+                    } else {
+                        return null;
+                    }
+                } catch (err) {
+                    console.error("Error fetching user: ", err);
+                    return null;
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.username = user.username;
+            }
+            return token;
+        },
+        async redirect({url, baseUrl}) {
+            if (url.startsWith("/"))
+                return `${baseUrl}${url}`;
+            else
+                return baseUrl;
+        },
+        async session({ session, token, user }) {
+            console.log(token);
+            session.accessToken = token.accessToken;
+            session.user.username = token.username;
+            return session;
+        }
+    },
+    secret: process.env.NEXTAUTH_SECRET
+};
+
+export default NextAuth(authOptions);
