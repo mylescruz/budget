e1597aa35b3351677732423cdaa288aaf248f467
Myles <mylesvcruz@gmail.com>
2025-02-26 16:47:53 -0800

    Added pre-rendering components for deployment of budget and income pages

--- pages/budget.js
+++ pages/budget.js
@@ -1,11 +1,21 @@
 import BudgetLayout from "@/components/budget/budgetLayout";
 import dateInfo from "@/helpers/dateInfo";
 import getMonthInfo from "@/helpers/getMonthInfo";
+import { Spinner } from "react-bootstrap";
 
 export default function Budget() {
     const month = dateInfo.currentMonth;
     const year = dateInfo.currentYear;
     const monthInfo = getMonthInfo(month, year);
+
+    if (!month) {
+        return (
+            <>
+                <h1 className="text-center">Loading budget</h1>
+                <Spinner animation="border" variant="primary" className="mx-auto" />
+            </>
+        );
+    }
 
     return (
         <BudgetLayout monthInfo={monthInfo} />
--- pages/income.js
+++ pages/income.js
@@ -1,8 +1,18 @@
 import PaystubLayout from "@/components/paystubs/paystubLayout";
 import dateInfo from "@/helpers/dateInfo";
+import { Spinner } from "react-bootstrap";
 
 export default function Income() {
     const year = dateInfo.currentYear;
+
+    if (!year) {
+        return (
+            <>
+                <h1 className="text-center">Loading user income</h1>
+                <Spinner animation="border" variant="primary" className="mx-auto" />
+            </>
+        );
+    }
 
     return (
         <PaystubLayout year={year} />
