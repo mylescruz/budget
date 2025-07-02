9ba4263d39c3d16e41c88fc5b755ce56cf806051
Myles <mylesvcruz@gmail.com>
2025-07-02 15:49:27 -0700

    Updated historyLayout to filter the table based on the current year

--- components/history/historyLayout.js
+++ components/history/historyLayout.js
@@ -1,7 +1,7 @@
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import HistoryTable from "./historyTable";
 import useHistory from "@/hooks/useHistory";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import dateInfo from "@/helpers/dateInfo";
 import getMonthInfo from "@/helpers/getMonthInfo";
 import { useSession } from "next-auth/react";
@@ -23,6 +23,14 @@ const HistoryLayout = () => {
     session.user.username,
     dateInfo.currentYear
   );
+  // const [historyYear, setHistoryYear] = useState(dateInfo.currentYear);
+  const [currentYearHistory, setCurrentYearHistory] = useState(history);
+  const [historyYears, setHistoryYears] = useState({
+    years: [],
+    current: dateInfo.currentYear,
+    max: dateInfo.currentYear,
+    min: dateInfo.currentYear,
+  });
 
   // If there is no user session, redirect to the home page
   if (!session) {
@@ -60,27 +68,89 @@ const HistoryLayout = () => {
     }
   }, [history, historyLoading, postHistory, getMonthHistory, getMonthIncome]);
 
+  // Get all the years in the history, the max, the min and the current year
+  useEffect(() => {
+    if (history) {
+      const years = new Set(history.map((historyMonth) => historyMonth.year));
+      const maxYear = Math.max(...years);
+      const minYear = Math.min(...years);
+
+      setHistoryYears({
+        years: years,
+        current: dateInfo.currentYear,
+        max: maxYear,
+        min: minYear,
+      });
+    }
+  }, [history]);
+
+  // Filter the history to the current year
+  useEffect(() => {
+    if (history) {
+      const givenYearHistory = history.filter(
+        (historyMonth) => historyMonth.year === historyYears.current
+      );
+
+      if (historyYears.current === dateInfo.currentYear) {
+        const filteredHistory = givenYearHistory.filter(
+          (historyMonth) => historyMonth.month !== dateInfo.currentMonth
+        );
+
+        setCurrentYearHistory(filteredHistory);
+      } else {
+        setCurrentYearHistory(givenYearHistory);
+      }
+    }
+  }, [history, historyYears]);
+
+  const nextYear = () => {
+    setHistoryYears({ ...historyYears, current: historyYears.current + 1 });
+  };
+
+  const previousYear = () => {
+    setHistoryYears({ ...historyYears, current: historyYears.current - 1 });
+  };
+
   // If the history is still being loaded by the API, show the loading component
   if (historyLoading) {
     return <Loading />;
+  } else {
+    return (
+      <>
+        <aside className="info-text text-center mx-auto">
+          <h1>History</h1>
+          <p>
+            View your budget versus what you actually spent for previous months.
+          </p>
+        </aside>
+
+        <Row className="d-flex">
+          <Col className="col-11 col-md-10 col-xl-8 mx-auto">
+            <HistoryTable history={currentYearHistory} />
+          </Col>
+        </Row>
+
+        <Row className="d-flex text-center">
+          <Col className="col-6">
+            <Button
+              onClick={previousYear}
+              disabled={historyYears.current === historyYears.min}
+            >
+              {historyYears.current - 1}
+            </Button>
+          </Col>
+          <Col className="col-6">
+            <Button
+              onClick={nextYear}
+              disabled={historyYears.current === historyYears.max}
+            >
+              {historyYears.current + 1}
+            </Button>
+          </Col>
+        </Row>
+      </>
+    );
   }
-
-  return (
-    <>
-      <aside className="info-text text-center mx-auto">
-        <h1>History</h1>
-        <p>
-          View your budget versus what you actually spent for previous months.
-        </p>
-      </aside>
-
-      <Row className="d-flex">
-        <Col className="col-11 col-md-10 col-xl-8 mx-auto">
-          <HistoryTable history={history} />
-        </Col>
-      </Row>
-    </>
-  );
 };
 
 export default HistoryLayout;
