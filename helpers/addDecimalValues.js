9badcbec3f5d51b98ed2dd87d616b1935cf6971d
Myles <mylesvcruz@gmail.com>
2025-12-27 22:20:13 -0600

    Create helper functions to add and subtract decimal values without dealing with floating-points for dollar values

---
+++ helpers/addDecimalValues.js
@@ -0,0 +1,3 @@
+export default function addDecimalValues(value1, value2) {
+  return value1 * 100 + value2 * 100;
+}
---
+++ helpers/subtractDecimalValues.js
@@ -0,0 +1,3 @@
+export default function subtractDefaultValues(value1, value2) {
+  return value1 * 100 - value2 * 100;
+}
