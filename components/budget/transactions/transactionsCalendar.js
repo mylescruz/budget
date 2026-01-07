5d74c5012479ca9fdc77534c6d7e991775d4c3b7
Myles <mylesvcruz@gmail.com>
2026-01-07 14:23:10 -0600

    refactor: Create a memo object for the transactions calendar to not rerender on every change and create an icon distinction between transactions and category transactions

--- components/budget/transactions/transactionsCalendar.js
+++ components/budget/transactions/transactionsCalendar.js
@@ -1,5 +1,5 @@
 import { TransactionsContext } from "@/contexts/TransactionsContext";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { Table } from "react-bootstrap";
 import styles from "@/styles/budget/transactions/transactionsCalendar.module.css";
 import { CategoriesContext } from "@/contexts/CategoriesContext";
@@ -11,112 +11,119 @@ const TransactionsCalendar = ({ dateInfo }) => {
   const { transactions } = useContext(TransactionsContext);
   const { categories, categoryColors } = useContext(CategoriesContext);
 
-  // Create the 2-D array for the given month
-  const monthNumber = dateInfo.month - 1;
-  const year = dateInfo.year;
-  const daysInMonth = parseInt(dateInfo.endOfMonth.split("-")[2]);
+  const monthCalendar = useMemo(() => {
+    // Create the 2-D array for the given month
+    const monthNumber = dateInfo.month - 1;
+    const year = dateInfo.year;
+    const daysInMonth = parseInt(dateInfo.endOfMonth.split("-")[2]);
 
-  let week = 0;
-  let month = [[]];
-  for (let dayIndex = 0; dayIndex < daysInMonth; dayIndex++) {
-    const date = new Date(year, monthNumber, dayIndex + 1);
-    const dateISO = date.toISOString().split("T")[0];
-    const dateNumber = date.getUTCDate();
-    const dayOfWeekIndex = date.getUTCDay();
+    let week = 0;
+    let month = [[]];
+    for (let dayIndex = 0; dayIndex < daysInMonth; dayIndex++) {
+      const date = new Date(year, monthNumber, dayIndex + 1);
+      const dateISO = date.toISOString().split("T")[0];
+      const dateNumber = date.getUTCDate();
+      const dayOfWeekIndex = date.getUTCDay();
 
-    // Fill up the month array with empty days until the first day of the month
-    if (dayIndex === 0 && dayOfWeekIndex !== 0) {
-      for (
-        let emptyDayIndex = 0;
-        emptyDayIndex < dayOfWeekIndex;
-        emptyDayIndex++
-      ) {
-        month[week].push({
-          dateNumber: null,
-          transactions: [],
-        });
+      // Fill up the month array with empty days until the first day of the month
+      if (dayIndex === 0 && dayOfWeekIndex !== 0) {
+        for (
+          let emptyDayIndex = 0;
+          emptyDayIndex < dayOfWeekIndex;
+          emptyDayIndex++
+        ) {
+          month[week].push({
+            dateNumber: null,
+            transactions: [],
+          });
+        }
       }
-    }
 
-    // Create a new array row at the start of each week
-    if (dayOfWeekIndex === 0) {
-      week += 1;
-      month.push([]);
-    }
+      // Create a new array row at the start of each week
+      if (dayOfWeekIndex === 0) {
+        week += 1;
+        month.push([]);
+      }
 
-    // Get all the transactions that occurred on that date and return the category's color
-    const dateTransactions = transactions
-      .filter((transaction) => transaction.date === dateISO)
-      .map((transaction) => {
-        return {
-          id: transaction._id,
-          color: categoryColors[transaction.category],
-        };
-      });
+      // Get all the transactions that occurred on that date and return the category's color
+      const dateTransactions = transactions
+        .filter((transaction) => transaction.date === dateISO)
+        .map((transaction) => {
+          return {
+            id: transaction._id,
+            color: categoryColors[transaction.category],
+            icon: "●",
+          };
+        });
 
-    // If dayOfMonth field is present in the fixed category or subcategory, add it to the transactions calendar
-    categories.forEach((category) => {
-      if (category.fixed) {
-        if (category.subcategories.length > 0) {
-          category.subcategories.forEach((subcategory) => {
-            if (subcategory.dayOfMonth) {
-              const subcategoryDate = new Date(
+      // If dayOfMonth field is present in the fixed category or subcategory, add it to the transactions calendar
+      categories.forEach((category) => {
+        if (category.fixed) {
+          if (category.subcategories.length > 0) {
+            category.subcategories.forEach((subcategory) => {
+              if (subcategory.dayOfMonth) {
+                const subcategoryDate = new Date(
+                  year,
+                  monthNumber,
+                  subcategory.dayOfMonth
+                );
+                const subcategoryDateISO = subcategoryDate
+                  .toISOString()
+                  .split("T")[0];
+
+                if (subcategoryDateISO === dateISO) {
+                  dateTransactions.push({
+                    id: subcategory.id,
+                    color: category.color,
+                    icon: "■",
+                  });
+                }
+              }
+            });
+          } else {
+            if (category.dayOfMonth) {
+              const categoryDate = new Date(
                 year,
                 monthNumber,
-                subcategory.dayOfMonth
+                category.dayOfMonth
               );
-              const subcategoryDateISO = subcategoryDate
-                .toISOString()
-                .split("T")[0];
+              const categoryDateISO = categoryDate.toISOString().split("T")[0];
 
-              if (subcategoryDateISO === dateISO) {
+              if (categoryDateISO === dateISO) {
                 dateTransactions.push({
-                  id: subcategory.id,
+                  id: category._id,
                   color: category.color,
+                  icon: "■",
                 });
               }
             }
-          });
-        } else {
-          if (category.dayOfMonth) {
-            const categoryDate = new Date(
-              year,
-              monthNumber,
-              category.dayOfMonth
-            );
-            const categoryDateISO = categoryDate.toISOString().split("T")[0];
-
-            if (categoryDateISO === dateISO) {
-              dateTransactions.push({
-                id: category._id,
-                color: category.color,
-              });
-            }
           }
         }
-      }
-    });
+      });
 
-    // Add the dateNumber and date's transactions to the calendar
-    month[week].push({
-      dateNumber,
-      transactions: dateTransactions,
-    });
+      // Add the dateNumber and date's transactions to the calendar
+      month[week].push({
+        dateNumber,
+        transactions: dateTransactions,
+      });
 
-    // At the end of the month, fill up the month array with empty days
-    if (dayIndex + 1 === daysInMonth && dayOfWeekIndex !== WEEK_LENGTH - 1) {
-      for (
-        let emptyDayIndex = dayOfWeekIndex;
-        emptyDayIndex < WEEK_LENGTH - 1;
-        emptyDayIndex++
-      ) {
-        month[week].push({
-          dateNumber: null,
-          transactions: [],
-        });
+      // At the end of the month, fill up the month array with empty days
+      if (dayIndex + 1 === daysInMonth && dayOfWeekIndex !== WEEK_LENGTH - 1) {
+        for (
+          let emptyDayIndex = dayOfWeekIndex;
+          emptyDayIndex < WEEK_LENGTH - 1;
+          emptyDayIndex++
+        ) {
+          month[week].push({
+            dateNumber: null,
+            transactions: [],
+          });
+        }
       }
     }
-  }
+
+    return month;
+  }, [dateInfo, categories, categoryColors, transactions]);
 
   return (
     <Table striped className={styles.equal_columns}>
@@ -131,7 +138,7 @@ const TransactionsCalendar = ({ dateInfo }) => {
         </tr>
       </thead>
       <tbody>
-        {month.map((week, weekIndex) => (
+        {monthCalendar.map((week, weekIndex) => (
           <tr key={weekIndex} className={styles.fixed_height}>
             {week.map((day, index) => (
               <td key={index}>
@@ -141,9 +148,9 @@ const TransactionsCalendar = ({ dateInfo }) => {
                     <span
                       key={transaction.id}
                       style={{ color: transaction.color }}
-                      className="mx-md-1 fs-4"
+                      className="mx-md-1 fs-2"
                     >
-                      ●
+                      {transaction.icon}
                     </span>
                   ))}
                 </div>
