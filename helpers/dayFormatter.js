f55d546de2259def7377775a734911a932712a20
Myles <mylesvcruz@gmail.com>
2026-01-12 13:05:28 -0600

    refactor: Create a programmatic function instead of a map for the dayFormatter

--- helpers/dayFormatter.js
+++ helpers/dayFormatter.js
@@ -1,37 +1,18 @@
-const dayMap = {
-  1: "1st",
-  2: "2nd",
-  3: "3rd",
-  4: "4th",
-  5: "5th",
-  6: "6th",
-  7: "7th",
-  8: "8th",
-  9: "9th",
-  10: "10th",
-  11: "11th",
-  12: "12th",
-  13: "13th",
-  14: "14th",
-  15: "15th",
-  16: "16th",
-  17: "17th",
-  18: "18th",
-  19: "19th",
-  20: "20th",
-  21: "21st",
-  22: "22nd",
-  23: "23rd",
-  24: "24th",
-  25: "25th",
-  26: "26th",
-  27: "27st",
-  28: "28th",
-  29: "29th",
-  30: "30th",
-  31: "31st",
-};
+export default function dayFormatter(day) {
+  if (day) {
+    if (day >= 11 && day <= 13) {
+      return `${day}th`;
+    }
 
-export default function dayFormatter(value) {
-  return dayMap[value];
+    switch (day % 10) {
+      case 1:
+        return `${day}st`;
+      case 2:
+        return `${day}nd`;
+      case 3:
+        return `${day}rd`;
+      default:
+        return `${day}th`;
+    }
+  }
 }
