8e4ca3847ddcb4c3eb4ab13dc97ea0273677f6d9
Myles <mylesvcruz@gmail.com>
2026-01-28 11:22:36 -0600

    fix: dashboard: Change summary year back to current year for users that newly created an account to not show last year's summary that they don't have

--- components/home/dashboard.js
+++ components/home/dashboard.js
@@ -31,8 +31,6 @@ const InnerDashboard = ({ dateInfo }) => {
 
   const [addTransactionClicked, setAddTransactionClicked] = useState(false);
 
-  const summaryYear = dateInfo.month === 1 ? dateInfo.year - 1 : dateInfo.year;
-
   // Get the top 5 categories to display on the dashboard
   const topCategories = useMemo(() => {
     if (!categories) {
@@ -170,13 +168,13 @@ const InnerDashboard = ({ dateInfo }) => {
               <Col className="col-12">
                 <Card className="my-2 card-background">
                   <Card.Body>
-                    <h4>{summaryYear} Summary</h4>
+                    <h4>{dateInfo.year} Summary</h4>
                     <p>View your total spending for the year</p>
                     <Button
                       as={Link}
                       href={{
                         pathname: "/summary",
-                        query: { year: summaryYear },
+                        query: { year: dateInfo.year },
                       }}
                       variant="primary"
                     >
