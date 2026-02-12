6f705a63362ab42bbb7ae33885730728e8aa125b
Myles <mylesvcruz@gmail.com>
2026-02-12 16:09:13 -0600

    feature: api: categories previous: Added all a category's fields to the returned previous categories array so a user has less manual input for already populated fields

--- pages/api/categories/previous/[year]/[month].js
+++ pages/api/categories/previous/[year]/[month].js
@@ -41,14 +41,74 @@ async function getPreviousCategories(req, res, { categoriesCol, username }) {
       (category) => category.name,
     );
 
-    // Get the name and color of all previous categories not currently in this month's categories
-    const previousCategories = await categoriesCol
-      .aggregate([
-        { $match: { username, name: { $nin: currentCategories } } },
-        { $group: { _id: { name: "$name", color: "$color" } } },
-        { $project: { _id: 0, name: "$_id.name", color: "$_id.color" } },
-      ])
+    // Get all user's categories that aren't in the current month
+    const allCategories = await categoriesCol
+      .find({ username, name: { $nin: currentCategories } })
       .toArray();
+
+    const previousCategoriesMap = new Map();
+
+    // Map the categories based on their name into a final formatted object
+    allCategories.forEach((category) => {
+      const exists = previousCategoriesMap.get(category.name);
+
+      if (!exists) {
+        const formattedCategory = {
+          name: category.name,
+          color: category.color,
+          budget: category.budget,
+          actual: 0,
+          fixed: category.fixed,
+        };
+
+        if (formattedCategory.fixed) {
+          formattedCategory.actual = category.actual;
+          formattedCategory.frequency = category.frequency;
+          formattedCategory.dueDate = category.dueDate;
+        }
+
+        formattedCategory.subcategories = category.subcategories.map(
+          (subcategory) => {
+            const formattedSubcategory = {
+              name: subcategory.name,
+              actual: 0,
+            };
+
+            if (category.fixed) {
+              formattedSubcategory.actual = subcategory.actual;
+              formattedSubcategory.frequency = subcategory.frequency;
+              formattedSubcategory.dueDate = subcategory.dueDate ?? 1;
+            }
+
+            return formattedSubcategory;
+          },
+        );
+
+        previousCategoriesMap.set(category.name, formattedCategory);
+      } else {
+        // If the category is already in the map, just add the subcategories to the item
+        category.subcategories.forEach((subcategory) => {
+          if (
+            !exists.subcategories.find((sub) => sub.name === subcategory.name)
+          ) {
+            const formattedSubcategory = {
+              name: subcategory.name,
+              actual: 0,
+            };
+
+            if (category.fixed) {
+              formattedSubcategory.actual = subcategory.actual;
+              formattedSubcategory.frequency = subcategory.frequency;
+              formattedSubcategory.dueDate = subcategory.dueDate ?? 1;
+            }
+
+            exists.subcategories.push(formattedSubcategory);
+          }
+        });
+      }
+    });
+
+    const previousCategories = [...previousCategoriesMap.values()];
 
     return res.status(200).json(previousCategories);
   } catch (error) {
