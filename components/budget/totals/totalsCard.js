91443e53a875e912938bf37d023bab250a6924dd
Myles <mylesvcruz@gmail.com>
2026-02-12 18:44:12 -0600

    feature: totalsCard: Add a color to the left to spend card so a user can visually see where they're spending is at and remove the total spent card to not confuse the user on the total amounts not adding up

--- components/budget/totals/totalsCard.js
+++ components/budget/totals/totalsCard.js
@@ -2,17 +2,19 @@ import PopUp from "@/components/layout/popUp";
 import dollarFormatter from "@/helpers/dollarFormatter";
 import { Card } from "react-bootstrap";
 
-const TotalsCard = ({ title, amount, description }) => {
+const TotalsCard = ({ title, amount, amountTextColor, description }) => {
   return (
-    <Card className="bg-dark text-white">
-      <Card.Body>
+    <Card className="bg-dark">
+      <Card.Body className="text-white">
         <h4 className="fw-bold">
           {title}
           <PopUp title={description}>
             <span className="fs-5"> &#9432;</span>
           </PopUp>
         </h4>
-        <h5>{dollarFormatter(amount)}</h5>
+        <h5 className={`${amountTextColor} fw-bold`}>
+          {dollarFormatter(amount)}
+        </h5>
       </Card.Body>
     </Card>
   );
--- components/budget/totals/totalsLayout.js
+++ components/budget/totals/totalsLayout.js
@@ -3,37 +3,55 @@ import { useContext } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import TotalsCard from "./totalsCard";
 
+const SAFE_LIMIT = Math.round((10 / 12) * 100);
+const MAX_VALUE = 100;
+
 const TotalsLayout = () => {
   const { categoryTotals } = useContext(CategoriesContext);
+
+  const nonFixedSpendingPercentage = Math.round(
+    (categoryTotals.nonFixedActual / categoryTotals.nonFixedBudget) * 100,
+  );
+
+  let nonFixedRemainingColor;
+
+  if (nonFixedSpendingPercentage >= MAX_VALUE || categoryTotals.budget === 0) {
+    nonFixedRemainingColor = "text-danger";
+  } else if (
+    nonFixedSpendingPercentage < MAX_VALUE &&
+    nonFixedSpendingPercentage > SAFE_LIMIT
+  ) {
+    nonFixedRemainingColor = "text-warning";
+  } else {
+    nonFixedRemainingColor = "text-success";
+  }
 
   const totals = [
     {
       title: "Total Budget",
       amount: categoryTotals.budget,
+      amountTextColor:
+        categoryTotals.budget === 0 ? "text-danger" : "text-white",
       description: "Your total income for the month.",
     },
     {
-      title: "Total Spent",
-      amount: categoryTotals.actual,
+      title: "Left to Spend",
+      amount: categoryTotals.nonFixedRemaining,
+      amountTextColor: nonFixedRemainingColor,
       description:
-        "The total amount spent between your fixed and changing expenses for the month.",
-    },
-    {
-      title: "Total Left",
-      amount: categoryTotals.remaining,
-      description:
-        "The total amount remaining for the month after all your expenses.",
+        "The amount remaining to spend after all your fixed expenses for the month are covered.",
     },
   ];
 
   return (
     <Container className="mb-4">
-      <Row className="text-center">
+      <Row className="text-center d-flex justify-content-evenly mx-auto">
         {totals.map((total) => (
-          <Col key={total.title} className="col-12 col-md-4 mb-2">
+          <Col key={total.title} className="col-12 col-md-5 mb-2">
             <TotalsCard
               title={total.title}
               amount={total.amount}
+              amountTextColor={total.amountTextColor}
               description={total.description}
             />
           </Col>
