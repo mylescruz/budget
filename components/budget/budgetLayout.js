9b542ca5d31425f57a29b4e8bff073f1e42c0c9c
Myles <mylesvcruz@gmail.com>
2026-02-18 08:18:41 -0600

    feature: budgetLayout: Add the useBudgetMonths hook and pagination in the UI for the budget to switch between previous, the current and future months budgets

--- components/budget/budgetLayout.js
+++ components/budget/budgetLayout.js
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import {
   CategoriesContext,
   CategoriesProvider,
@@ -13,6 +13,8 @@ import {
 import LoadingIndicator from "../layout/loadingIndicator";
 import TotalsLayout from "./totals/totalsLayout";
 import CategoryTableLayout from "./categoryTableLayout/categoryTableLayout";
+import useBudgetMonths from "@/hooks/useBudgetMonths";
+import getDateInfo from "@/helpers/getDateInfo";
 
 const InnerBudgetLayout = ({ dateInfo }) => {
   const { categories, categoriesLoading } = useContext(CategoriesContext);
@@ -32,17 +34,6 @@ const InnerBudgetLayout = ({ dateInfo }) => {
   } else {
     return (
       <Container className="w-100">
-        <aside className="info-text mx-auto text-center">
-          <h1>
-            {dateInfo.monthName} {dateInfo.year}
-          </h1>
-          <p className="fs-6">
-            Set your budget for your fixed and changing expenses. Log all your
-            transactions made this month. See how much you spent based on the
-            category.
-          </p>
-        </aside>
-
         <Row className="d-flex justify-content-center">
           <Col className="col-12 col-xl-10">
             <TotalsLayout />
@@ -65,13 +56,103 @@ const InnerBudgetLayout = ({ dateInfo }) => {
 };
 
 const BudgetLayout = ({ dateInfo }) => {
-  return (
-    <CategoriesProvider dateInfo={dateInfo}>
-      <TransactionsProvider dateInfo={dateInfo}>
-        <InnerBudgetLayout dateInfo={dateInfo} />
-      </TransactionsProvider>
-    </CategoriesProvider>
-  );
+  const [monthInfo, setMonthInfo] = useState(dateInfo);
+
+  const { budgetMonths, budgetMonthsLoading } = useBudgetMonths();
+
+  const nextMonth = () => {
+    let monthNum = monthInfo.month + 1;
+    let yearNum = monthInfo.year;
+
+    if (monthNum > 12) {
+      monthNum = 1;
+      yearNum += 1;
+    }
+
+    const date = new Date(`${monthNum}/01/${yearNum}`);
+    const info = getDateInfo(date);
+
+    setMonthInfo(info);
+  };
+
+  const previousMonth = () => {
+    let monthNum = monthInfo.month - 1;
+    let yearNum = monthInfo.year;
+
+    if (monthNum === 0) {
+      monthNum = 12;
+      yearNum -= 1;
+    }
+
+    const date = new Date(`${monthNum}/01/${yearNum}`);
+    const info = getDateInfo(date);
+
+    setMonthInfo(info);
+  };
+
+  if (budgetMonthsLoading) {
+    return <LoadingIndicator />;
+  } else if (!budgetMonths) {
+    return (
+      <Row className="mt-4 text-center">
+        <p className="fw-bold text-danger">
+          &#9432; There was an error loading your budget months. Please try
+          again later!
+        </p>
+      </Row>
+    );
+  } else {
+    return (
+      <CategoriesProvider dateInfo={monthInfo}>
+        <TransactionsProvider dateInfo={monthInfo}>
+          <div className="mx-auto">
+            <Row className="d-flex col-12 col-md-8 col-lg-6 col-xl-5 justify-items-between mx-auto align-items-center text-center">
+              <Col className="col-2">
+                <Button
+                  onClick={previousMonth}
+                  size="sm"
+                  className="btn-dark fw-bold"
+                  disabled={
+                    budgetMonths.current.month === budgetMonths.min.month &&
+                    budgetMonths.current.year === budgetMonths.min.year
+                  }
+                >
+                  &#60;
+                </Button>
+              </Col>
+              <Col className="col-8">
+                <h1 className="p-0 m-0 fw-bold">
+                  {monthInfo.monthName} {monthInfo.year}
+                </h1>
+              </Col>
+              <Col className="col-2">
+                <Button
+                  onClick={nextMonth}
+                  size="sm"
+                  className="btn-dark fw-bold"
+                  disabled={
+                    budgetMonths.current.month === budgetMonths.max.month &&
+                    budgetMonths.current.year === budgetMonths.max.year
+                  }
+                >
+                  &#62;
+                </Button>
+              </Col>
+            </Row>
+            <Container className="my-2 mx-auto text-center">
+              <p className="fs-6">
+                Set your budget for your fixed and changing expenses. Log all
+                your transactions made this month. See how much you spent based
+                on the category.
+              </p>
+            </Container>
+
+            <InnerBudgetLayout dateInfo={monthInfo} />
+          </div>
+        </TransactionsProvider>
+      </CategoriesProvider>
+    );
+  }
 };
 
 export default BudgetLayout;
