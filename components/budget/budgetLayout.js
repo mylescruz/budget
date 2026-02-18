802bad093d1adeb7455e835b8184154f032747f1
Myles <mylesvcruz@gmail.com>
2026-02-18 12:04:49 -0600

    feature: budgetMonthSwitcher: Create a component to switch between the budget months and replace the current switching months logic in the budget page with this budgetMonthSwitcher component

--- components/budget/budgetLayout.js
+++ components/budget/budgetLayout.js
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import {
   CategoriesContext,
   CategoriesProvider,
@@ -13,8 +13,7 @@ import {
 import LoadingIndicator from "../layout/loadingIndicator";
 import TotalsLayout from "./totals/totalsLayout";
 import CategoryTableLayout from "./categoryTableLayout/categoryTableLayout";
-import useBudgetMonths from "@/hooks/useBudgetMonths";
-import getDateInfo from "@/helpers/getDateInfo";
+import BudgetMonthSwitcher from "../layout/budgetMonthSwitcher";
 
 const InnerBudgetLayout = ({ dateInfo }) => {
   const { categories, categoriesLoading } = useContext(CategoriesContext);
@@ -58,101 +57,25 @@ const InnerBudgetLayout = ({ dateInfo }) => {
 const BudgetLayout = ({ dateInfo }) => {
   const [monthInfo, setMonthInfo] = useState(dateInfo);
 
-  const { budgetMonths, budgetMonthsLoading } = useBudgetMonths();
-
-  const nextMonth = () => {
-    let monthNum = monthInfo.month + 1;
-    let yearNum = monthInfo.year;
-
-    if (monthNum > 12) {
-      monthNum = 1;
-      yearNum += 1;
-    }
-
-    const date = new Date(`${monthNum}/01/${yearNum}`);
-    const info = getDateInfo(date);
-
-    setMonthInfo(info);
+  const pageInfo = {
+    title: `${monthInfo.monthName} ${monthInfo.year}`,
+    description:
+      "Set your budget for your fixed and changing expenses. Log all your transactions made this month. See how much you spent based on the category.",
   };
 
-  const previousMonth = () => {
-    let monthNum = monthInfo.month - 1;
-    let yearNum = monthInfo.year;
-
-    if (monthNum === 0) {
-      monthNum = 12;
-      yearNum -= 1;
-    }
-
-    const date = new Date(`${monthNum}/01/${yearNum}`);
-    const info = getDateInfo(date);
-
-    setMonthInfo(info);
-  };
-
-  if (budgetMonthsLoading) {
-    return <LoadingIndicator />;
-  } else if (!budgetMonths) {
-    return (
-      <Row className="mt-4 text-center">
-        <p className="fw-bold text-danger">
-          &#9432; There was an error loading your budget months. Please try
-          again later!
-        </p>
-      </Row>
-    );
-  } else {
-    return (
-      <CategoriesProvider dateInfo={monthInfo}>
-        <TransactionsProvider dateInfo={monthInfo}>
-          <div className="mx-auto">
-            <Row className="d-flex col-12 col-md-8 col-lg-6 col-xl-5 justify-items-between mx-auto align-items-center text-center">
-              <Col className="col-2">
-                <Button
-                  onClick={previousMonth}
-                  size="sm"
-                  className="btn-dark fw-bold"
-                  disabled={
-                    budgetMonths.current.month === budgetMonths.min.month &&
-                    budgetMonths.current.year === budgetMonths.min.year
-                  }
-                >
-                  &#60;
-                </Button>
-              </Col>
-              <Col className="col-8">
-                <h1 className="p-0 m-0 fw-bold">
-                  {monthInfo.monthName} {monthInfo.year}
-                </h1>
-              </Col>
-              <Col className="col-2">
-                <Button
-                  onClick={nextMonth}
-                  size="sm"
-                  className="btn-dark fw-bold"
-                  disabled={
-                    budgetMonths.current.month === budgetMonths.max.month &&
-                    budgetMonths.current.year === budgetMonths.max.year
-                  }
-                >
-                  &#62;
-                </Button>
-              </Col>
-            </Row>
-            <Container className="my-2 mx-auto text-center">
-              <p className="fs-6">
-                Set your budget for your fixed and changing expenses. Log all
-                your transactions made this month. See how much you spent based
-                on the category.
-              </p>
-            </Container>
-
-            <InnerBudgetLayout dateInfo={monthInfo} />
-          </div>
-        </TransactionsProvider>
-      </CategoriesProvider>
-    );
-  }
+  return (
+    <CategoriesProvider dateInfo={monthInfo}>
+      <TransactionsProvider dateInfo={monthInfo}>
+        <BudgetMonthSwitcher
+          monthInfo={monthInfo}
+          setMonthInfo={setMonthInfo}
+          pageInfo={pageInfo}
+        >
+          <InnerBudgetLayout dateInfo={monthInfo} />
+        </BudgetMonthSwitcher>
+      </TransactionsProvider>
+    </CategoriesProvider>
+  );
 };
 
 export default BudgetLayout;
---
+++ components/layout/budgetMonthSwitcher.js
@@ -0,0 +1,96 @@
+import useBudgetMonths from "@/hooks/useBudgetMonths";
+import { Button, Col, Row } from "react-bootstrap";
+import LoadingIndicator from "./loadingIndicator";
+import getDateInfo from "@/helpers/getDateInfo";
+
+const BudgetMonthSwitcher = ({
+  monthInfo,
+  setMonthInfo,
+  pageInfo,
+  children,
+}) => {
+  const { budgetMonths, budgetMonthsLoading } = useBudgetMonths();
+
+  const previousMonth = () => {
+    let monthNum = monthInfo.month - 1;
+    let yearNum = monthInfo.year;
+
+    if (monthNum === 0) {
+      monthNum = 12;
+      yearNum -= 1;
+    }
+
+    const date = new Date(`${monthNum}/01/${yearNum}`);
+    const info = getDateInfo(date);
+
+    setMonthInfo(info);
+  };
+  const nextMonth = () => {
+    let monthNum = monthInfo.month + 1;
+    let yearNum = monthInfo.year;
+
+    if (monthNum > 12) {
+      monthNum = 1;
+      yearNum += 1;
+    }
+
+    const date = new Date(`${monthNum}/01/${yearNum}`);
+    const info = getDateInfo(date);
+
+    setMonthInfo(info);
+  };
+
+  if (budgetMonthsLoading) {
+    return <LoadingIndicator />;
+  } else if (!budgetMonths) {
+    return (
+      <Row className="mt-4 text-center">
+        <p className="fw-bold text-danger">
+          &#9432; There was an error loading your budget months. Please try
+          again later!
+        </p>
+      </Row>
+    );
+  } else {
+    return (
+      <div className="mx-auto">
+        <Row className="d-flex col-12 col-md-8 col-lg-6 col-xl-5 justify-items-between mx-auto align-items-center text-center">
+          <Col className="col-2">
+            <Button
+              onClick={previousMonth}
+              size="sm"
+              className="btn-dark fw-bold"
+              disabled={
+                budgetMonths.current.month === budgetMonths.min.month &&
+                budgetMonths.current.year === budgetMonths.min.year
+              }
+            >
+              &#60;
+            </Button>
+          </Col>
+          <Col className="col-8">
+            <h1 className="p-0 m-0 fw-bold">{pageInfo.title}</h1>
+          </Col>
+          <Col className="col-2">
+            <Button
+              onClick={nextMonth}
+              size="sm"
+              className="btn-dark fw-bold"
+              disabled={
+                budgetMonths.current.month === budgetMonths.max.month &&
+                budgetMonths.current.year === budgetMonths.max.year
+              }
+            >
+              &#62;
+            </Button>
+          </Col>
+        </Row>
+        <p className="my-2 mx-4 text-center">{pageInfo.description}</p>
+
+        <div className="mt-4">{children}</div>
+      </div>
+    );
+  }
+};
+
+export default BudgetMonthSwitcher;
