52184e47b7ffa8aace8f41e5c5b2a9e040596c2b
Myles <mylesvcruz@gmail.com>
2026-02-19 11:09:24 -0600

    feature: categoryBadge: Adjust how many characters of a category's name displays based on the user's device

--- components/category/categoryBadge.js
+++ components/category/categoryBadge.js
@@ -4,11 +4,19 @@ const CategoryBadge = ({ name, color }) => {
     border: color,
   };
 
-  const formattedName = name.length < 14 ? name : name.slice(0, 14) + "...";
+  const xsName = name.length > 13 ? name.slice(0, 10) + "..." : name;
+  const smName = name.length > 20 ? name.slice(0, 17) + "..." : name;
+  const mdName = name.length > 16 ? name.slice(0, 13) + "..." : name;
+  const lgName = name.length > 18 ? name.slice(0, 15) + "..." : name;
+  const xlName = name.length > 15 ? name.slice(0, 12) + "..." : name;
 
   return (
     <span style={badgeStyle} className="badge fw-bold fs-6 text-white">
-      {formattedName}
+      <span className="d-sm-none">{xsName}</span>
+      <span className="d-none d-sm-block d-md-none">{smName}</span>
+      <span className="d-none d-md-block d-lg-none">{mdName}</span>
+      <span className="d-none d-lg-block d-xl-none">{lgName}</span>
+      <span className="d-none d-xl-block">{xlName}</span>
     </span>
   );
 };
