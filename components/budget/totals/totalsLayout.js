936de1d98e5582e606a553791c997af6d6d8209f
Myles <mylesvcruz@gmail.com>
2026-03-07 10:31:02 -0600

    feature: totalsLayout: Calculate the transfers in and out of the user's account and reflect those numbers in the available funds to spend and the amount left to spend for the month

--- components/budget/totals/totalsLayout.js
+++ components/budget/totals/totalsLayout.js
@@ -1,47 +1,77 @@
 import { CategoriesContext } from "@/contexts/CategoriesContext";
 import { useContext } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import TotalsCard from "./totalsCard";
+import { TransactionsContext } from "@/contexts/TransactionsContext";
+import dollarsToCents from "@/helpers/dollarsToCents";
+import centsToDollars from "@/helpers/centsToDollars";
 
-const SAFE_LIMIT = Math.round((10 / 12) * 100);
-const MAX_VALUE = 100;
+const WARNING_PERCENTAGE = 10;
 
 const TotalsLayout = () => {
   const { categoryTotals } = useContext(CategoriesContext);
+  const { transactions } = useContext(TransactionsContext);
 
-  // Define the text color of the amount values for the cards
-  const nonFixedSpendingPercentage = Math.round(
-    (categoryTotals.nonFixedActual / categoryTotals.nonFixedBudget) * 100,
+  // Calculate the money transferred in and out of a user's account
+  const transfers = transactions.reduce(
+    (sum, current) => {
+      if (current.type === "Transfer") {
+        if (current.toAccount === "Checking") {
+          sum.in += dollarsToCents(current.amount);
+        }
+
+        if (current.toAccount === "Savings") {
+          sum.out += dollarsToCents(current.amount);
+        }
+      }
+
+      return sum;
+    },
+    { in: 0, out: 0 },
   );
 
-  let nonFixedRemainingColor;
+  const availableFunds = centsToDollars(
+    dollarsToCents(categoryTotals.budget) + transfers.in,
+  );
+
+  const leftoverVariableFunds = centsToDollars(
+    dollarsToCents(availableFunds) -
+      dollarsToCents(categoryTotals.fixedBudget) -
+      dollarsToCents(categoryTotals.nonFixedActual) -
+      transfers.out,
+  );
+
+  // Define the text color of the amount values for the cards
+  const variableSpendingPercentage = Math.round(
+    (leftoverVariableFunds / availableFunds) * 100,
+  );
+
+  let leftoverFundsColor;
 
   // Show red text if the user has no income or if their available spending balance is less than 0
-  if (categoryTotals.budget === 0 || categoryTotals.nonFixedRemaining < 0) {
-    nonFixedRemainingColor = "text-danger";
+  if (variableSpendingPercentage <= 0 && leftoverVariableFunds <= 0) {
+    leftoverFundsColor = "text-danger";
   } else if (
-    nonFixedSpendingPercentage < MAX_VALUE &&
-    nonFixedSpendingPercentage > SAFE_LIMIT
+    variableSpendingPercentage >= 0 &&
+    variableSpendingPercentage < WARNING_PERCENTAGE
   ) {
-    nonFixedRemainingColor = "text-warning";
+    leftoverFundsColor = "text-warning";
   } else {
-    nonFixedRemainingColor = "text-success";
+    leftoverFundsColor = "text-success";
   }
 
   const totals = [
     {
-      title: "Total Budget",
-      amount: categoryTotals.budget,
-      amountTextColor:
-        categoryTotals.budget === 0 ? "text-danger" : "text-white",
-      description: "Your total income for the month.",
+      title: "Total Funds",
+      amount: availableFunds,
+      amountTextColor: availableFunds === 0 ? "text-danger" : "text-white",
+      description: "Monthly income plus incoming transfers.",
     },
     {
-      title: "Left to Spend",
-      amount: categoryTotals.nonFixedRemaining,
-      amountTextColor: nonFixedRemainingColor,
-      description:
-        "The amount remaining to spend after all your fixed expenses for the month are covered.",
+      title: "Available to Spend",
+      amount: leftoverVariableFunds,
+      amountTextColor: leftoverFundsColor,
+      description: "Available to spend after all expenses and transfers.",
     },
   ];
 
