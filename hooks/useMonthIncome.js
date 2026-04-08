2af50f0031992119caf4753dbcc96683ccd07a13
Myles <mylesvcruz@gmail.com>
2026-04-08 09:15:41 -0500

    refactor: useMonthIncome: Add an monthIncomeRequest state object to track the status of the GET request

--- hooks/useMonthIncome.js
+++ hooks/useMonthIncome.js
@@ -1,28 +1,51 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const useMonthIncome = (month, year) => {
   const [monthIncome, setMonthIncome] = useState(0);
   const [monthIncomeLoading, setMonthIncomeLoading] = useState(true);
+  const [monthIncomeRequest, setMonthIncomeRequest] = useState({
+    action: null, //  get | null
+    status: "idle", // idle | loading | success | error
+    message: null,
+  });
 
   useEffect(() => {
-    const getMonthIncome = async (month, year) => {
-      try {
-        const rsp = await fetch(`/api/monthIncome/${year}/${month}`);
-
-        const result = await rsp.text();
-        setMonthIncome(parseFloat(result));
-      } catch (error) {
-        setMonthIncome(null);
-        console.error(error);
-      } finally {
-        setMonthIncomeLoading(false);
-      }
-    };
-
     getMonthIncome(month, year);
   }, [month, year]);
 
-  return { monthIncome, monthIncomeLoading };
+  const getMonthIncome = useCallback(async (month, year) => {
+    setMonthIncomeRequest({
+      action: "get",
+      status: "loading",
+      message: "Getting your income for the month",
+    });
+
+    try {
+      const rsp = await fetch(`/api/monthIncome/${year}/${month}`);
+
+      const result = await rsp.text();
+      setMonthIncome(Number(result));
+
+      setMonthIncomeRequest({
+        action: "get",
+        status: "success",
+        message: null,
+      });
+    } catch (error) {
+      setMonthIncome(null);
+      console.error(error);
+
+      setMonthIncomeRequest({
+        action: "get",
+        status: "error",
+        message: error.message,
+      });
+    } finally {
+      setMonthIncomeLoading(false);
+    }
+  }, []);
+
+  return { monthIncome, monthIncomeLoading, monthIncomeRequest };
 };
 
 export default useMonthIncome;
