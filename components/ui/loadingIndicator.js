105b907339fb809fe4873f7f1ed369fe7dba3d0b
Myles <mylesvcruz@gmail.com>
2026-04-08 18:33:41 -0500

    feature: loadingIndicator: Add a message field to give context to the loading indicator

--- components/ui/loadingIndicator.js
+++ components/ui/loadingIndicator.js
@@ -1,9 +1,10 @@
 import { Spinner } from "react-bootstrap";
 
-export default function LoadingIndicator() {
+export default function LoadingIndicator({ message }) {
   return (
-    <div className="d-flex justify-content-center align-items-center loading-spinner">
+    <div className="d-flex flex-column justify-content-center align-items-center loading-spinner">
       <Spinner animation="border" variant="primary" />
+      <p className="text-center mt-4 fs-6">{message}</p>
     </div>
   );
 }
