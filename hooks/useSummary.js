ec0b0784fe07a14bb8a0df9d56bd06edb3bf98b6
Myles <mylesvcruz@gmail.com>
2026-04-08 09:35:09 -0500

    feature: useSummary: Add a summaryRequest state object to track the status of the GET request

--- hooks/useSummary.js
+++ hooks/useSummary.js
@@ -1,35 +1,58 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const useSummary = (year) => {
   const [summary, setSummary] = useState([]);
   const [summaryLoading, setSummaryLoading] = useState(true);
+  const [summaryRequest, setSummaryRequest] = useState({
+    action: null, //  get | null
+    status: "idle", // idle | loading | success | error
+    message: null,
+  });
 
   useEffect(() => {
-    const getSummary = async () => {
-      setSummaryLoading(true);
-
-      try {
-        const response = await fetch(`/api/summary/${year}`);
-
-        if (response.ok) {
-          const fetchedSummary = await response.json();
-          setSummary(fetchedSummary);
-        } else {
-          const message = await response.text();
-          throw new Error(message);
-        }
-      } catch (error) {
-        setSummary(null);
-        console.error(error);
-      } finally {
-        setSummaryLoading(false);
-      }
-    };
-
-    getSummary();
+    getSummary(year);
   }, [year]);
 
-  return { summary, summaryLoading };
+  const getSummary = useCallback(async (year) => {
+    setSummaryLoading(true);
+    setSummaryRequest({
+      action: "get",
+      status: "loading",
+      message: `Getting your budget summary for ${year}`,
+    });
+
+    try {
+      const response = await fetch(`/api/summary/${year}`);
+
+      if (!response.ok) {
+        const message = await response.text();
+        throw new Error(message);
+      }
+
+      const fetchedSummary = await response.json();
+
+      setSummary(fetchedSummary);
+
+      setSummaryRequest({
+        action: "get",
+        status: "success",
+        message: null,
+      });
+    } catch (error) {
+      setSummaryRequest({
+        action: "get",
+        status: "error",
+        message: error.message,
+      });
+
+      setSummary(null);
+      console.error(error);
+    } finally {
+      setSummaryLoading(false);
+    }
+  }, []);
+
+  return { summary, summaryLoading, summaryRequest };
 };
 
 export default useSummary;
