60adc4ad2173036cd3b92c15224898026957d693
Myles <mylesvcruz@gmail.com>
2026-04-08 07:55:22 -0500

    feature: useBudgetYears: Add a request state object that tracks the status of API calls

--- hooks/useBudgetYears.js
+++ hooks/useBudgetYears.js
@@ -1,34 +1,64 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+
+// Fetches each year a user has had a budget stored in the app
+// GET response
+/*
+  {
+    years: [year],
+    current,
+    max,
+    min
+  };
+*/
 
 const useBudgetYears = () => {
   const [budgetYears, setBudgetYears] = useState([]);
   const [budgetYearsLoading, setBudgetYearsLoading] = useState(true);
+  const [budgetYearsReq, setBudgetYearsReq] = useState({
+    status: "idle", // idle | loading | success | error
+    message: null,
+  });
 
   useEffect(() => {
-    const getYears = async () => {
-      try {
-        const response = await fetch("api/budgetYears");
-
-        if (response.ok) {
-          const fetchedYears = await response.json();
-
-          setBudgetYears(fetchedYears);
-        } else {
-          const message = await response.text();
-          throw new Error(message);
-        }
-      } catch (error) {
-        setBudgetYears(null);
-        console.error(error);
-      } finally {
-        setBudgetYearsLoading(false);
-      }
-    };
-
     getYears();
   }, []);
 
-  return { budgetYears, budgetYearsLoading };
+  const getYears = useCallback(async () => {
+    setBudgetYearsReq({
+      status: "loading",
+      message: "Getting the years in your budget",
+    });
+
+    try {
+      const response = await fetch("api/budgetYears");
+
+      if (!response.ok) {
+        const message = await response.text();
+        throw new Error(message);
+      }
+
+      const fetchedYears = await response.json();
+
+      setBudgetYears(fetchedYears);
+
+      setBudgetYearsReq({
+        status: "success",
+        message: null,
+      });
+    } catch (error) {
+      setBudgetYears(null);
+
+      setBudgetYearsReq({
+        status: "error",
+        message: error.message,
+      });
+      console.error(error);
+    } finally {
+      setBudgetYearsLoading(false);
+    }
+  }, []);
+
+  return { budgetYears, budgetYearsLoading, budgetYearsReq };
 };
 
 export default useBudgetYears;
