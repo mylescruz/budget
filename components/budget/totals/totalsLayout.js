eb50bc79038cb217a87a7f7f2896f46c324bdd92
Myles <mylesvcruz@gmail.com>
2026-05-04 11:59:53 -0500

    feature: Update the totalsLayout design to match the categories and transactions design

--- components/budget/totals/totalsLayout.js
+++ components/budget/totals/totalsLayout.js
@@ -3,13 +3,11 @@ import { Card, Col, Row } from "react-bootstrap";
 import dollarsToCents from "@/helpers/dollarsToCents";
 import centsToDollars from "@/helpers/centsToDollars";
 import dollarFormatter from "@/helpers/dollarFormatter";
-import PopUp from "@/components/ui/popUp";
-import ProgressBar from "@/components/ui/progressBar";
 import addDecimalValues from "@/helpers/addDecimalValues";
 import ErrorMessage from "@/components/ui/errorMessage";
 import { BudgetContext } from "@/contexts/BudgetContext";
 
-const WARNING_PERCENTAGE = 10;
+const WARNING_PERCENTAGE = 0.9;
 
 const TotalsLayout = () => {
   const { categoryTotals, transactionTotals } = useContext(BudgetContext);
@@ -20,10 +18,11 @@ const TotalsLayout = () => {
       return null;
     }
 
-    const availableFunds = addDecimalValues(
-      transactionTotals.income,
-      transactionTotals.checkingTransfers,
-    );
+    const income = transactionTotals.income;
+
+    const transfersIn = transactionTotals.checkingTransfers;
+
+    const availableFunds = addDecimalValues(income, transfersIn);
 
     const currentSpending = addDecimalValues(
       categoryTotals.actual,
@@ -39,15 +38,13 @@ const TotalsLayout = () => {
 
     let leftToSpendTextColor;
 
-    const variableSpentPercent = Math.round(
-      (leftToSpend / availableFunds) * 100,
-    );
+    const variableSpentPercent = leftToSpend / availableFunds;
 
     if (variableSpentPercent <= 0 && leftToSpend <= 0) {
       leftToSpendTextColor = "text-danger";
     } else if (
       variableSpentPercent >= 0 &&
-      variableSpentPercent < WARNING_PERCENTAGE
+      variableSpentPercent >= WARNING_PERCENTAGE
     ) {
       leftToSpendTextColor = "text-warning";
     } else {
@@ -55,9 +52,11 @@ const TotalsLayout = () => {
     }
 
     return {
+      income,
+      transfersIn,
       availableFunds,
       availableFundsTextColor:
-        availableFunds === 0 ? "text-danger" : "text-white",
+        availableFunds === 0 ? "text-danger" : "text-dark",
       currentSpending,
       leftToSpend,
       leftToSpendTextColor,
@@ -67,75 +66,106 @@ const TotalsLayout = () => {
     };
   }, [categoryTotals, transactionTotals]);
 
+  const percentSpent = totals.currentSpending / totals.availableFunds;
+
+  let spentColor;
+  let remainingText;
+  const isOver = totals.leftToSpend < 0;
+
+  if (percentSpent <= 0 && totals.leftToSpend <= 0) {
+    spentColor = "text-danger";
+    remainingText = "You're overspending!";
+  } else if (percentSpent >= 0 && percentSpent >= WARNING_PERCENTAGE) {
+    spentColor = "text-warning";
+    remainingText = "Close to limit";
+  } else {
+    spentColor = "text-success";
+    remainingText = "On track";
+  }
+
   return (
     <div className="mb-4">
       {totals ? (
         <>
-          <Row className="mb-3">
-            <Col className="col-12 col-md-6 my-1">
-              <Card className="text-center bg-dark text-white p-3">
-                <h5>
-                  Available Funds{" "}
-                  <PopUp
-                    id="availableFunds"
-                    title={"Monthly income plus incoming transfers."}
-                  >
-                    <span className="fs-6"> &#9432;</span>
-                  </PopUp>
-                </h5>
-                <h2 className={totals.availableFundsTextColor}>
-                  {dollarFormatter(totals.availableFunds)}
-                </h2>
-              </Card>
-            </Col>
-            <Col className="col-12 col-md-6 my-1">
-              <Card className="text-center bg-dark p-3">
-                <h5 className="text-white">
-                  Left to Spend
-                  <PopUp
-                    id="leftToSpend"
-                    title={
-                      "Available to spend after all expenses and transfers."
-                    }
-                  >
-                    <span className="fs-6"> &#9432;</span>
-                  </PopUp>
-                </h5>
-                <h2 className={totals.leftToSpendTextColor}>
-                  {dollarFormatter(totals.leftToSpend)}
-                </h2>
-              </Card>
-            </Col>
-          </Row>
+          <Card className="shadow-sm border-0 rounded-4 mb-4">
+            <Card.Body>
+              <Row className="align-items-center">
+                <Col>
+                  <h2 className={`fw-bold ${totals.leftToSpendTextColor}`}>
+                    {dollarFormatter(totals.leftToSpend)} Remaining
+                  </h2>
 
-          <ProgressBar
-            currentValue={totals.currentSpending}
-            totalValue={totals.availableFunds}
-          />
-
-          <Row className="mt-4 text-center">
-            <Col className="col-4 my-1">
-              <PopUp
-                id="variableSpent"
-                title={"Total spent on expense transactions"}
-              >
-                <div>Expenses: {dollarFormatter(totals.variableActual)}</div>
-              </PopUp>
-            </Col>
-            <Col className="col-4 my-1">
-              <PopUp
-                id="fixedCategories"
-                title={"Total obligation for all fixed categories"}
-              >
-                <div>Fixed Bills: {dollarFormatter(totals.fixedBudget)}</div>
-              </PopUp>
-            </Col>
-            <Col className="col-4 my-1">
-              <PopUp id="saved" title={"Total transferred out to savings"}>
-                <div>Saved: {dollarFormatter(totals.savingsTransfers)}</div>
-              </PopUp>
-            </Col>
-          </Row>
+                  <div className="d-flex gap-4 mt-3 flex-column flex-lg-row justify-content-between">
+                    <div className="d-flex flex-row">
+                      <div className="mx-2">
+                        <div className="text-muted small">Available Funds</div>
+                        <div className="fw-semibold">
+                          {dollarFormatter(totals.availableFunds)}
+                        </div>
+                      </div>
+                      <div className="mx-2">
+                        <div className="text-muted small">Current Expenses</div>
+                        <div className="fw-semibold">
+                          {dollarFormatter(totals.currentSpending)} (
+                          {Math.round(percentSpent * 100)}%)
+                        </div>
+                      </div>
+                    </div>
+                    <div className="d-none d-md-flex flex-row">
+                      <div className="mx-2">
+                        <div className="text-muted small">Income</div>
+                        <div
+                          className={`fw-semibold ${totals.availableFundsTextColor}`}
+                        >
+                          {dollarFormatter(totals.income)}
+                        </div>
+                      </div>
+                      {totals.transfersIn > 0 && (
+                        <div className="mx-2">
+                          <div className="text-muted small">Transfers In</div>
+                          <div className="fw-semibold ${totals.transfersIn">
+                            {dollarFormatter(totals.transfersIn)}
+                          </div>
+                        </div>
+                      )}
+                      <div className="mx-2">
+                        <div className="text-muted small">Saved</div>
+                        <div className="fw-semibold">
+                          {dollarFormatter(totals.savingsTransfers)}
+                        </div>
+                      </div>
+                      <div className="mx-2">
+                        <div className="text-muted small">Expenses</div>
+                        <div className="fw-semibold">
+                          {dollarFormatter(totals.variableActual)}
+                        </div>
+                      </div>
+                      <div className="mx-2">
+                        <div className="text-muted small">Fixed Bils</div>
+                        <div className="fw-semibold">
+                          {dollarFormatter(totals.fixedBudget)}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+              <div className="mt-2">
+                <div className="progress" style={{ height: 6 }}>
+                  <div
+                    className={`progress-bar ${
+                      isOver
+                        ? "bg-danger"
+                        : percentSpent > 90
+                          ? "bg-warning"
+                          : "bg-success"
+                    }`}
+                    style={{ width: `${percentSpent * 100}%` }}
+                  />
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
         </>
       ) : (
         <ErrorMessage
