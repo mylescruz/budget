e08bc2c28e39f8c19ffcf39d355266a4d6efde4c
Myles <mylesvcruz@gmail.com>
2026-05-08 11:04:45 -0500

    feat: Redesign the budgetMonthSwitcher buttons to match the rest of the budget page layout

--- components/ui/budgetMonthSwitcher.js
+++ components/ui/budgetMonthSwitcher.js
@@ -38,34 +38,44 @@ const BudgetMonthSwitcher = ({
 
   // Function to move one month back
   const previousMonth = () => {
-    let monthNum = monthInfo.month - 1;
-    let yearNum = monthInfo.year;
+    if (
+      monthInfo.month !== budgetMonths.min.month ||
+      monthInfo.year !== budgetMonths.min.year
+    ) {
+      let monthNum = monthInfo.month - 1;
+      let yearNum = monthInfo.year;
 
-    if (monthNum === 0) {
-      monthNum = 12;
-      yearNum -= 1;
+      if (monthNum === 0) {
+        monthNum = 12;
+        yearNum -= 1;
+      }
+
+      const date = new Date(`${monthNum}/01/${yearNum}`);
+      const info = getDateInfo(date);
+
+      setMonthInfo(info);
     }
-
-    const date = new Date(`${monthNum}/01/${yearNum}`);
-    const info = getDateInfo(date);
-
-    setMonthInfo(info);
   };
 
   // Function to move one month forward
   const nextMonth = () => {
-    let monthNum = monthInfo.month + 1;
-    let yearNum = monthInfo.year;
+    if (
+      monthInfo.year !== budgetMonths.max.year ||
+      (monthInfo.year === budgetMonths.max.year && monthInfo.month < 12)
+    ) {
+      let monthNum = monthInfo.month + 1;
+      let yearNum = monthInfo.year;
 
-    if (monthNum > 12) {
-      monthNum = 1;
-      yearNum += 1;
+      if (monthNum > 12) {
+        monthNum = 1;
+        yearNum += 1;
+      }
+
+      const date = new Date(`${monthNum}/01/${yearNum}`);
+      const info = getDateInfo(date);
+
+      setMonthInfo(info);
     }
-
-    const date = new Date(`${monthNum}/01/${yearNum}`);
-    const info = getDateInfo(date);
-
-    setMonthInfo(info);
   };
 
   if (
@@ -80,23 +90,19 @@ const BudgetMonthSwitcher = ({
           {budgetMonths ? (
             <Row className="d-flex col-12 col-md-8 col-lg-6 col-xl-5 justify-items-between mx-auto align-items-center text-center">
               <Col className="col-2 col-lg-1">
-                <Button
+                <i
+                  className="bi bi-chevron-left clicker"
                   onClick={previousMonth}
-                  size="sm"
-                  className="btn-dark fw-bold"
-                  disabled={
-                    monthInfo.month === budgetMonths.min.month &&
-                    monthInfo.year === budgetMonths.min.year
-                  }
-                >
-                  &#60;
-                </Button>
+                />
               </Col>
               <Col className="col-8 col-lg-10 px-0">
                 <div className="d-flex justify-content-center align-items-center">
                   <h1 className={styles.title}>{pageInfo.title}</h1>
-                  <Dropdown className="mx-2 mx-md-3 mx-lg-3">
-                    <Dropdown.Toggle variant="dark" size="sm" />
+                  <Dropdown>
+                    <Dropdown.Toggle
+                      variant="light"
+                      className="border-0 bg-transparent"
+                    />
                     <Dropdown.Menu className={styles.menu}>
                       {budgetMonths.months.map((month) => (
                         <div key={`${month.month}/${month.year}`}>
@@ -123,17 +129,10 @@ const BudgetMonthSwitcher = ({
                 </div>
               </Col>
               <Col className="col-2 col-lg-1">
-                <Button
+                <i
+                  className="bi bi-chevron-right clicker"
                   onClick={nextMonth}
-                  size="sm"
-                  className="btn-dark fw-bold"
-                  disabled={
-                    monthInfo.month === budgetMonths.max.month &&
-                    monthInfo.year === budgetMonths.max.year
-                  }
-                >
-                  &#62;
-                </Button>
+                />
               </Col>
             </Row>
           ) : (
@@ -142,7 +141,9 @@ const BudgetMonthSwitcher = ({
               <ErrorMessage message={budgetMonthsRequest.message} />
             </div>
           )}
-          <p className="my-2 mx-4 text-center">{pageInfo.description}</p>
+          <p className="my-2 mx-4 text-muted small text-center">
+            {pageInfo.description}
+          </p>
 
           <div className="mt-4">{children}</div>
         </div>
