a8f156b2e75639dab425e7d25e2276e8e621dd7b
Myles <mylesvcruz@gmail.com>
2026-06-26 09:27:49 -0500

    feat: Create the POST request in the debts api endpoint to add a new debt to the database

--- pages/api/debts/index.js
+++ pages/api/debts/index.js
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 import clientPromise from "@/lib/mongodb";
 import { logError } from "@/lib/logError";
+import dollarsToCents from "@/helpers/dollarsToCents";
+import { DEBT_TYPE } from "@/lib/constants/debt";
 
 export default async function handler(req, res) {
   // Establish NextAuth.js session
@@ -24,6 +26,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getDebts(req, res, debtsContext);
+    case "POST":
+      return addDebt(req, res, debtsContext);
     default:
       return res.status(405).send(`${req.method} method not allowed`);
   }
@@ -44,6 +48,63 @@ async function getDebts(req, res, { debtsCol, username }) {
       .status(500)
       .send(
         "We're unable to fetch your debt at the moment. Please try again later!",
+      );
+  }
+}
+
+// Add a user's new debt to MongoDB
+async function addDebt(req, res, { debtsCol, username }) {
+  const debtInfo = req.body;
+
+  const currentTS = new Date();
+
+  try {
+    // Format the new debt
+    const newDebt = {
+      username,
+      type: debtInfo.type,
+      lender: debtInfo.lender,
+      active: true,
+      currentBalance: dollarsToCents(debtInfo.currentBalance),
+      apr: Number(debtInfo.apr),
+      notes: debtInfo.notes,
+      createdTS: currentTS,
+      updatedTS: currentTS,
+    };
+
+    // Add specific fields for Loan type debt
+    if (newDebt.type === DEBT_TYPE.LOAN) {
+      newDebt.originalBalance = dollarsToCents(debtInfo.originalBalance);
+      newDebt.startDate = new Date(debtInfo.startDate);
+      newDebt.targetPayoffDate = new Date(debtInfo.targetPayoffDate);
+    }
+
+    // Add specific fields for Credit Card type debt
+    if (newDebt.type === DEBT_TYPE.CREDIT_CARD) {
+      newDebt.creditLimit = dollarsToCents(debtInfo.creditLimit);
+      newDebt.promoAPR =
+        debtInfo.promoAPR === "" ? null : Number(debtInfo.promoAPR);
+      newDebt.promoAPREndDate =
+        debtInfo.promoAPREndDate === ""
+          ? null
+          : new Date(debtInfo.promoAPREndDate);
+    }
+
+    let insertedDebt;
+
+    // Insert the new debt into MongoDB
+    insertedDebt = await debtsCol.insertOne(newDebt, {
+      maxTimeMS: 5000,
+    });
+
+    return res.status(200).json({ _id: insertedDebt.insertedId, ...newDebt });
+  } catch (error) {
+    await logError({ error, req, username });
+
+    return res
+      .status(500)
+      .send(
+        "We're unable to add your new debt at the moment. Please try again later!",
       );
   }
 }
